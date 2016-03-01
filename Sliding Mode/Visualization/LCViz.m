7fab7d6e15e239995484f29d0f008f736bb5e1bb
Logan <giantpandagod@gmail.com>
2016-02-29 22:56:05 -0600

    Updated visualization plots
    Added pendulum visualization and updated LC to include correct surface
    dynamics.

--- Sliding Mode/Visualization/LCViz.m
+++ Sliding Mode/Visualization/LCViz.m
@@ -1,47 +1,50 @@
+%Initialize
 clear; close all; clc;
+%simulation & controller input
+lambda = 1;
 
-lambda = -1;
-tf = 20;
-
-L=0.1;
-C=1/L;
-
-eIn = 1;
+%system parameters
+L = 0.001;     %H
+C = 1/L; %F
+eIn = 3.3;
+%solution parameters voltage
 eD = eIn/2;
-
-
+tf = 10;
+dt = 0.001;
+%system equations
 x1dot = @(x1, x2) x2;
-x2dot = @(x1, x2, ea) 1/(L*C)*(ea - x1);
-
+x2dot = @(x1, x2, ea) -1/(L*C)*(x1 - ea);
+%% PLOT 1 - System Space
 [x1, x2] = meshgrid(-5:0.25:5, -5:0.25:5);
-
+%Switch is closed (ea = eIn)
 B = eIn;
-x1d = x1dot(x1, x2) .* (((-lambda*x1)+x2)<0+eD);
-x2d = x2dot(x1, x2, B) .* (((-lambda*x1)+x2)<0+eD);
-
+%half of system equations
+x1d = x1dot(x1, x2) .* ( (lambda*(-x2) + (eD-x1)) > 0 );
+x2d = x2dot(x1, x2, B) .* ( (lambda*(-x2) + (eD-x1)) > 0 );
+%plot system
 size = sqrt(x1d.^2 + x2d.^2);
 quiver(x1, x2, x1d./size, x2d./size, 'c');
-
-%%
+%lower half of system space
 clear x1d x2d;
-
+%switch is open (ea = 0)
 B = 0;
-x1d = x1dot(x1, x2) .* (((-lambda*x1)+x2)>0+eD);
-x2d = x2dot(x1, x2, B) .* (((-lambda*x1)+x2)>0+eD);
+%half of the system space
+x1d = x1dot(x1, x2) .* ( (lambda*(-x2) + (eD-x1)) < 0 );
+x2d = x2dot(x1, x2, B) .* ( (lambda*(-x2) + (eD-x1)) < 0 );
 size = sqrt(x1d.^2 + x2d.^2);
+%plot 2nd half
 hold on
 quiver(x1, x2, x1d./size, x2d./size, 'r');
 axis square
 xlabel('eC');
 ylabel('eC-Dot');
-
+title('System Response');
 %% plot vs time
-ec0 = 3; ecDot0 = 1.5;
+ec0 = 0; ecDot0 = 0;
 
 
-s = @(ec, ecDot) (((-lambda)*ec + ecDot) <= eD)*eIn;
+s = @(ec, ecDot) ( (lambda*(-ecDot) + (eD-ec)) >= 0 )*eIn;
 
-dt = 0.01;
 t = 0:dt:tf;
 
 ec(1) = ec0;
@@ -69,3 +72,31 @@ plot(ec, ecDot, '.-k')
 
 
 
+%% ERROR SPACE PLOT
+clear x1d x2d;
+
+x1dotE = @(x1, x2) x2;
+x2dotE = @(x1, x2, ea) 1/(L*C)*(eD - x1 - ea);
+
+
+[x1, x2] = meshgrid(-5:0.25:5, -5:0.25:5);
+
+B = eIn;
+x1d = x1dotE(x1, x2) .* (x1 + lambda*x2 > 0);
+x2d = x2dotE(x1, x2, B) .* (x1 + lambda*x2 > 0);
+
+size = sqrt(x1d.^2 + x2d.^2);
+figure();
+quiver(x1, x2, x1d./size, x2d./size, 'c');
+
+clear x1d x2d;
+B = 0;
+x1d = x1dotE(x1, x2) .* (x1 + lambda*x2 < 0);
+x2d = x2dotE(x1, x2, B) .* (x1 + lambda*x2 < 0);
+size = sqrt(x1d.^2 + x2d.^2);
+hold on
+quiver(x1, x2, x1d./size, x2d./size, 'r');
+axis square
+xlabel('eCE');
+ylabel('eCE-Dot');
+title('System Response in Error Space');
---
+++ Sliding Mode/Visualization/PendulumViz.m
@@ -0,0 +1,101 @@
+clear; close all; clc;
+
+%controller variables
+lambda = 1.75;  % s - 1/slope of sliding surface
+beta = 4;       % N-M - Torque Magnitude
+%solution variables
+tf = 20;
+thD = 2;    % rad - desired angle
+%system parameters
+g = 9.81;   % m/s2 - gravity
+L = 1;      % m - pendulum length
+m = 0.25;   % kg - mass of end
+%system equations
+x1dot = @(x1, x2) x2;
+x2dot = @(x1, x2, T) -g/L*sin(x1) + 1/(m*L^2)*T;
+%% phase plane plot solution space 1
+[x1, x2] = meshgrid(-5:0.25:5, -5:0.25:5);
+
+Tin = beta;
+x1d = x1dot(x1, x2) .* ( ((thD-x1) + lambda*(-x2)) > 0 );
+x2d = x2dot(x1, x2, Tin) .* ( ((thD-x1) + lambda*(-x2)) > 0 );
+
+size = sqrt(x1d.^2 + x2d.^2);
+quiver(x1, x2, x1d./size, x2d./size, 'c');
+
+% phase plane plot solution space 2
+clear x1d x2d;
+
+Tin = -beta;
+x1d = x1dot(x1, x2) .* ( ((thD-x1) + lambda*(-x2)) < 0 );
+x2d = x2dot(x1, x2, Tin) .* ( ((thD+-x1) + lambda*(-x2)) < 0 );
+size = sqrt(x1d.^2 + x2d.^2);
+hold on
+quiver(x1, x2, x1d./size, x2d./size, 'r');
+%plot axis setup
+axis square
+xlabel('th');
+ylabel('th-Dot');
+
+
+%% plot vs time
+th0 = -2; thDot0 = -3;
+
+s = @(th, thDot) sign(((lambda)*(-thDot) + (thD - th)))*beta;
+
+dt = 0.01;
+t = 0:dt:tf;
+
+th(1) = th0;
+thDot(1) = thDot0;
+
+for i = 2:length(t)
+    K1 = x1dot(th(i-1), thDot(i-1))*dt;
+    L1 = x2dot(th(i-1), thDot(i-1), s(th(i-1), thDot(i-1)))*dt;
+    
+    K2 = x1dot(th(i-1) + K1/2, thDot(i-1) + L1/2)*dt;
+    L2 = x2dot(th(i-1) + K1/2, thDot(i-1) + L1/2, s(th(i-1), thDot(i-1)))*dt;
+    
+    K3 = x1dot(th(i-1) + K2/2, thDot(i-1) + L2/2)*dt;
+    L3 = x2dot(th(i-1) + K1/2, thDot(i-1) + L1/2, s(th(i-1), thDot(i-1)))*dt;
+    
+    K4 = x1dot(th(i-1) + K3  , thDot(i-1) + L3)*dt;
+    L4 = x2dot(th(i-1) + K1/2, thDot(i-1) + L1/2, s(th(i-1), thDot(i-1)))*dt;
+    
+    th(i) = th(i-1) + 1/6*K1 + 1/3*K2 + 1/3*K3 + 1/6*K4;
+    thDot(i) = thDot(i-1) + 1/6*L1 + 1/3*L2 + 1/3*L3 + 1/6*L4;
+    
+end
+
+plot(th, thDot, '.-k')
+
+
+%% phase plane plot solution space 1 - ERROR SPACE
+x1dotE = @(x1, x2) x2;
+x2dotE = @(x1, x2, T) g/L*sin(thD - x1) - 1/(m*L^2)*T;
+
+[x1, x2] = meshgrid(-5:0.25:5, -5:0.25:5);
+
+clear x1d x2d;
+Tin = beta;
+x1d = x1dotE(x1, x2) .* ( (lambda*x2 + x1) > 0 );
+x2d = x2dotE(x1, x2, Tin) .* ( (lambda*x2 + x1) > 0 );
+
+size = sqrt(x1d.^2 + x2d.^2);
+figure(2);
+quiver(x1, x2, x1d./size, x2d./size, 'c');
+
+% phase plane plot solution space 2 - ERROR SPACE
+clear x1d x2d;
+
+Tin = -beta;
+x1d = x1dotE(x1, x2) .* ( (lambda*x2 + x1) < 0 );
+x2d = x2dotE(x1, x2, Tin) .* ( (lambda*x2 + x1) < 0 );
+size = sqrt(x1d.^2 + x2d.^2);
+hold on
+quiver(x1, x2, x1d./size, x2d./size, 'r');
+%plot axis setup
+axis square
+xlabel('th');
+ylabel('th-Dot');
+
